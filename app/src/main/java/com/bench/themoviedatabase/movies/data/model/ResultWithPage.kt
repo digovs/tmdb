d7c6740195297518fde22ecf08437e5c19b84fa8
Kevin Li <kli@CL-MMA5111.local>
2021-09-23 13:30:14 -0400

    update field name for ResultWithPage

--- app/src/main/java/com/bench/themoviedatabase/movies/data/model/ResultWithPage.kt
+++ app/src/main/java/com/bench/themoviedatabase/movies/data/model/ResultWithPage.kt
@@ -3,6 +3,6 @@ package com.bench.themoviedatabase.movies.data.model
 data class ResultWithPage<T>(
     val page: Int,
     val results: List<T>,
-    val total_pages: Int,
-    val total_result: Int
+    val totalPages: Int,
+    val totalResult: Int
 )
