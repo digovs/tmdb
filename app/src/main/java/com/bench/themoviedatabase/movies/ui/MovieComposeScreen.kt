28542fc7168ba13e839c3e52c45196bcbf1933de
Kevin Li <kli@CL-MMA5111.local>
2021-10-01 16:05:01 -0400

    remove data provider for preview

--- app/src/main/java/com/bench/themoviedatabase/movies/ui/MovieComposeScreen.kt
+++ app/src/main/java/com/bench/themoviedatabase/movies/ui/MovieComposeScreen.kt
@@ -9,18 +9,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.bench.themoviedatabase.R
+import com.bench.themoviedatabase.movies.data.model.MovieItem
 import com.bench.themoviedatabase.movies.data.model.MovieItemsWithSection
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 @Composable
-@Preview
 fun MovieScreen(
-    @PreviewParameter(FakeMovieListViewModelProvider::class) viewModel: IMovieListViewModel
+    viewModel: MovieListViewModel
 ) {
-    val listOfMovies = viewModel.recentMovieItemsLiveData.observeAsState()
-    val lazyMovieItems = viewModel.genresWithMoviesFlow.collectAsLazyPagingItems()
+    MovieContent(movies = viewModel.recentMovieItemsLiveData, genreSections = viewModel.genresWithMoviesFlow)
+}
+
+@Composable
+fun MovieContent(
+    movies: LiveData<List<MovieItem>>,
+    genreSections: Flow<PagingData<MovieItemsWithSection>>
+){
+    val listOfMovies = movies.observeAsState()
+    val lazyMovieItems = genreSections.collectAsLazyPagingItems()
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -42,4 +54,21 @@ fun MovieScreen(
                 }
             }
         })
+}
+
+@Composable
+@Preview
+fun PreviewHomeContent(){
+    val recentMovieItemsLiveData = MutableLiveData<List<MovieItem>>(
+        listOf(
+            MovieItem(title = "title1", releaseDate = "release date 1"),
+            MovieItem(title = "title2", releaseDate = "release date 2"),
+            MovieItem(title = "title3", releaseDate = "release date 3")
+        )
+    )
+
+    // cannot preview paging data
+    val genresWithMoviesFlow: Flow<PagingData<MovieItemsWithSection>> = emptyFlow()
+
+    MovieContent(movies = recentMovieItemsLiveData, genreSections = genresWithMoviesFlow)
 }
